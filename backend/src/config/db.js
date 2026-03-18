1ba52545e1c49b9035c41668eeb1453618ae6d17
kavinnnnnnnnn <sriramponraj4057@gmail.com>
2026-03-18 15:56:25 +0530

    chore: update database credentials for Railway

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,19 @@
 const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql"
-  }
-)
+const dbUrl = process.env.MYSQL_PUBLIC_URL || null;
+
+const sequelize = dbUrl 
+  ? new Sequelize(dbUrl, {
+      dialect: "mysql"
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        host: process.env.DB_HOST,
+        dialect: "mysql"
+      }
+    );
 
 module.exports = sequelize
